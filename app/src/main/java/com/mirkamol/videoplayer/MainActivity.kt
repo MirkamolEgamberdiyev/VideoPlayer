b016d9332535c5fbc2523da19346d3498ad2451d
Mirkamol <mirkamol9187907@gmail.com>
2022-01-05 11:46:44 +0500

    Play Video ready

--- app/src/main/java/com/mirkamol/videoplayer/MainActivity.kt
+++ app/src/main/java/com/mirkamol/videoplayer/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.mirkamol.videoplayer
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.khizar1556.mkvideoplayer.MKPlayer
+import com.khizar1556.mkvideoplayer.MKPlayerActivity
 import net.alhazmy13.mediapicker.Video.VideoPicker
 
 class MainActivity : AppCompatActivity() {
@@ -41,8 +44,9 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
-            val videoPaths = data!!.getIntegerArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH)
-            Log.d("videoPaths", "onActivityResult: $videoPaths")
+            val videoPaths = data!!.getStringArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH)
+
+            MKPlayerActivity.configPlayer(this@MainActivity).play(videoPaths?.get(0).toString())
         }
     }
 
