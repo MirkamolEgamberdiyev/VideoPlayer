0e3233b8bd657e20f0685e6587dcd23c0027a5fd
Mirkamol <mirkamol9187907@gmail.com>
2022-01-05 11:28:10 +0500

    VideoPicker worked

--- app/src/main/java/com/mirkamol/videoplayer/MainActivity.kt
+++ app/src/main/java/com/mirkamol/videoplayer/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.mirkamol.videoplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import net.alhazmy13.mediapicker.Video.VideoPicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        initViews()
+
+
     }
+
+
+    private fun initViews() {
+        val playVideo = findViewById<Button>(R.id.btn_play_video)
+
+        playVideo.setOnClickListener {
+            pickVideo()
+        }
+    }
+
+    private fun pickVideo() {
+        val videoPicker = VideoPicker.Builder(this@MainActivity)
+            .mode(VideoPicker.Mode.GALLERY)
+            .directory(VideoPicker.Directory.DEFAULT)
+            .extension(VideoPicker.Extension.MP4)
+            .enableDebuggingMode(true)
+
+        videoPicker.build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            val videoPaths = data!!.getIntegerArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH)
+            Log.d("videoPaths", "onActivityResult: $videoPaths")
+        }
+    }
+
 }
